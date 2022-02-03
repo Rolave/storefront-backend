1d8c823af71e49d1fe7c6abd0d2d71def136a31f
rholave@gmail.com <Ro7190ol!>
2022-02-03 18:17:36 -0300

    feat: create utilities

---
+++ src/utilities/index.ts
@@ -0,0 +1,1 @@
+export { verifyAuthToken } from './verifyAuthToken';
---
+++ src/utilities/verifyAuthToken.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from 'express';
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
+
+dotenv.config();
+
+const { SECRET_TOKEN: secretToken } = process.env;
+
+export const verifyAuthToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const authorizationHeader = req.headers.authorization as string;
+    const token = authorizationHeader.split(' ')[1];
+
+    jwt.verify(token, secretToken as string);
+    next();
+  } catch (error) {
+    res.status(401);
+    res.json('Access denied, invalid token');
+  }
+};
